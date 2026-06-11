5a385cdacbafe82d38e366d57f94e548a953558e
Leandro Alves <tk.leandro@gmail.com>
2026-06-11 11:53:24 -0300

    feat(catalogo): adiciona exceções MaterialDominioException e ServicoDominioEexception

---
+++ src/main/java/br/com/genciv/catalogo/domain/exception/ServicoDominioException.java
@@ -0,0 +1,7 @@
+package br.com.genciv.catalogo.domain.exception;
+
+public class ServicoDominioException extends RuntimeException {
+    public ServicoDominioException(String message) {
+        super(message);
+    }
+}
