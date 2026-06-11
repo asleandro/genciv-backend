1c8437088c15a2de2765e786a96b396dcb2f3daf
Leandro Alves <tk.leandro@gmail.com>
2026-06-10 21:00:59 -0300

    feat(catalogo) adiciona enum UnidadeMedida

---
+++ src/main/java/br/com/genciv/catalogo/domain/enums/UnidadeMedida.java
@@ -0,0 +1,24 @@
+package br.com.genciv.catalogo.domain.enums;
+
+public enum UnidadeMedida {
+    M,
+    M2,
+    M3,
+    KG,
+    RL,
+    BD,
+    CX,
+    VB,
+    CJ,
+    PC,
+    UNID,
+    CENTO,
+    SACO,
+    PACOTE,
+    PALETE,
+    CART,
+    BOBINA,
+    LATA,
+    LITRO,
+    MILHEI
+}
